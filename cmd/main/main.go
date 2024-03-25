0ad9a677b35f11853ef81dd792cd886ad6617129
Mutiara Enjelina <mutiaramanullang77@gmail.com>
2024-03-25 16:44:43 +0700

    commit1

---
+++ cmd/main/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+
+	"github.com/Mutiaraenjelina/go_students_crud_mysql/pkg/routes"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+func main() {
+    r := mux.NewRouter()
+    routes.RegisterStudentsRoutes(r)
+    http.Handle("/", r)
+    fmt.Print("Starting Server on localhost:9010")
+    log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
